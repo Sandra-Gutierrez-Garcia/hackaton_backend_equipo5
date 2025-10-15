5b37eeb5dbd25a19ecd3567bfa0ff28f092302a4
Marc Sanchez <mks4nch3z@gmail.com>
2025-10-15 16:34:43 +0200

    calculating co2 emission per plane

--- laravel/app/Http/Controllers/PlaneController.php
+++ laravel/app/Http/Controllers/PlaneController.php
@@ -6,8 +6,7 @@ use App\Models\PlaneModel;
 use Exception;
 use Illuminate\Http\Request;
 
-class PlaneController extends Controller
-{
+class PlaneController extends Controller{
     public function index(){
         try{
             $planes = PlaneModel::all();
@@ -67,13 +66,28 @@ class PlaneController extends Controller
         }
     }
 
-    // n movimientos entrada/salida.
-    //En la salida, el despegue gasta un porcentaje de combustible
+    // n movimientos entrada/salida = aprox 500 entradas y 500 salidas diarias
+    //n movimientos al mes son 1000 * 30 = 30000 movimientos.
+    //aprox se genera 2.5Kgs / 1L gasolina
+    //En el despegue y ascenso inicial, el gasto de combustible es de un 7% aprox
     //Cuanto Co2 emite 1L o 1 de fuel_capacity
     //calcular el porcentaje de combustible gastado en el despegue, multiplicar por co2 emitido por litro
     //sumar todos los movimientos de salida. 
 
     //El resultado es la contaminación emitida por los despegues de aviones en un aeropuerto.
     //En el territorio se suma, su nivel de polucion base + co2 emitido por las salidas de los aviones
-}
 
+    public function calculateOnePlanePollution($id){
+        $planes = PlaneModel::findOrFail($id);
+        $pollution = 0;
+        $planes->each(function($plane) use (&$pollution){
+            $pollution += ($plane->capacity * 0.07) * 2.5;
+        });
+
+        return response()->json([
+            'success' => true,
+            'data' => $pollution
+        ], 200);
+    }
+
+}
