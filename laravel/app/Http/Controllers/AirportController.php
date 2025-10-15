772153075f105a37e35b549c0ec1bf4edf52d180
Sandra <lacereza7@gmail.com>
2025-10-15 18:09:44 +0200

    AiportController Completado

--- laravel/app/Http/Controllers/AirportController.php
+++ laravel/app/Http/Controllers/AirportController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Models\AirportModel;
 
 class AirportController extends Controller
 {
     //
+    public function index(): JsonResponse
+    {
+        try{
+            $airport = AiportModel::getAllAirports();  
+            return response()->json([
+                'success' => true,
+                'data' => $airport,
+                'count' => $airport->count()
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message'=>'Error retrieving airports',
+                'error' => $e->getMessage(),
+            ],500);
+          }
+    }
+    public function show($id): jsonResponse
+    {
+        try{
+             $airport = AirportModel::getAirportById($id);
+             if(!$airport){
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Airport not found'
+                ], 404);
+             }
+                return response()->json([
+                    'success' => true,
+                    'data' => $airport
+                ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error retrieving airport',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
