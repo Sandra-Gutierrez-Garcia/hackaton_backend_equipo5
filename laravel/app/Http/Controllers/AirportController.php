469feec6e09f82fee49494c57c9f738c9155cb02
Sandra <lacereza7@gmail.com>
2025-10-15 16:20:03 +0200

    endpoint de aiport y method show y controller

--- laravel/app/Http/Controllers/AirportController.php
+++ laravel/app/Http/Controllers/AirportController.php
@@ -7,4 +7,41 @@ use Illuminate\Http\Request;
 class AirportController extends Controller
 {
     //
+    public function index(){
+        try{
+            return response()->json([
+                'message' => 'list of airports',
+                'data' => Airport::all()
+            ], 200);
+
+        } catch(\Exception $e){
+            return response()->json([
+                'message' => 'Error retrieving airports',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function show($id){
+
+        try{
+            $aiport = Aiport::find($id);
+            if(!$aiport){
+                return response()->json([
+                    'message' => 'Airport not found',
+                ], 404);
+            }
+
+            return response()->json([
+                'message' => 'Airport found',
+                'data' => $aiport
+            ],200);
+
+        } catch(\Exception $e){
+            return response()->json([
+                'message' => 'Error retrieving airports',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
